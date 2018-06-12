5aeafb349de61a44b8aea12248d01c974dc7bced
eynar-pari-gmail <eynar.pari@gmail.com>
2018-06-11 21:50:50 -0400

    adsdas

---
+++ src/main/java/com/mycompany/myapp/domain/extras/BimestreDTO.java
@@ -0,0 +1,508 @@
+package com.mycompany.myapp.domain.extras;
+
+public class BimestreDTO {
+
+    private Long ci;
+    private String nombre;
+    private String paterno;
+    private String materno;
+    private String genero;
+    private String fechaNacimiento;
+    private String nacionalidad;
+    private String direccion;
+    private Long telefono;
+    private String matricula;
+    private String tipo;
+    private Long idDocente;
+    private Integer bimestre;
+    private String paralelo;
+    private Integer gestion;
+    private Long idMateria;
+    private String observacion;
+    private String indicadorCualitativo;
+    private Double notaBimestralFinal;
+    private Double promedioAutoevaluacion;
+    private Double autoevaluacionDecir;
+    private Double autoevaluacionSer;
+    private Double decirPromedio;
+    private Double hacerPromedio;
+    private Double saberPromedio;
+    private Double serPromedio;
+    private Integer notaSer1;
+    private Integer notaSer2;
+    private Integer notaSer3;
+    private Integer notaSer4;
+    private Integer notaSer5;
+    private Integer notaSer6;
+    private Integer notaSaber1;
+    private Integer notaSaber2;
+    private Integer notaSaber3;
+    private Integer notaSaber4;
+    private Integer notaSaber5;
+    private Integer notaSaber6;
+    private Integer notaHacer1;
+    private Integer notaHacer2;
+    private Integer notaHacer3;
+    private Integer notaHacer4;
+    private Integer notaHacer5;
+    private Integer notaHacer6;
+    private Integer notaDecir1;
+    private Integer notaDecir2;
+    private Integer notaDecir3;
+    private Integer notaDecir4;
+    private Integer notaDecir5;
+    private Integer notaDecir6;
+
+    public BimestreDTO(Long ci, String nombre, String paterno, String materno, String genero, String fechaNacimiento, String nacionalidad, String direccion, Long telefono, String matricula, String tipo, Long idDocente, Integer bimestre, String paralelo, Integer gestion, Long idMateria, String observacion, String indicadorCualitativo, Double notaBimestralFinal, Double promedioAutoevaluacion, Double autoevaluacionDecir, Double autoevaluacionSer, Double decirPromedio, Double hacerPromedio, Double saberPromedio, Double serPromedio, Integer notaSer1, Integer notaSer2, Integer notaSer3, Integer notaSer4, Integer notaSer5, Integer notaSer6, Integer notaSaber1, Integer notaSaber2, Integer notaSaber3, Integer notaSaber4, Integer notaSaber5, Integer notaSaber6, Integer notaHacer1, Integer notaHacer2, Integer notaHacer3, Integer notaHacer4, Integer notaHacer5, Integer notaHacer6, Integer notaDecir1, Integer notaDecir2, Integer notaDecir3, Integer notaDecir4, Integer notaDecir5, Integer notaDecir6) {
+        this.ci = ci;
+        this.nombre = nombre;
+        this.paterno = paterno;
+        this.materno = materno;
+        this.genero = genero;
+        this.fechaNacimiento = fechaNacimiento;
+        this.nacionalidad = nacionalidad;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.matricula = matricula;
+        this.tipo = tipo;
+        this.idDocente = idDocente;
+        this.bimestre = bimestre;
+        this.paralelo = paralelo;
+        this.gestion = gestion;
+        this.idMateria = idMateria;
+        this.observacion = observacion;
+        this.indicadorCualitativo = indicadorCualitativo;
+        this.notaBimestralFinal = notaBimestralFinal;
+        this.promedioAutoevaluacion = promedioAutoevaluacion;
+        this.autoevaluacionDecir = autoevaluacionDecir;
+        this.autoevaluacionSer = autoevaluacionSer;
+        this.decirPromedio = decirPromedio;
+        this.hacerPromedio = hacerPromedio;
+        this.saberPromedio = saberPromedio;
+        this.serPromedio = serPromedio;
+        this.notaSer1 = notaSer1;
+        this.notaSer2 = notaSer2;
+        this.notaSer3 = notaSer3;
+        this.notaSer4 = notaSer4;
+        this.notaSer5 = notaSer5;
+        this.notaSer6 = notaSer6;
+        this.notaSaber1 = notaSaber1;
+        this.notaSaber2 = notaSaber2;
+        this.notaSaber3 = notaSaber3;
+        this.notaSaber4 = notaSaber4;
+        this.notaSaber5 = notaSaber5;
+        this.notaSaber6 = notaSaber6;
+        this.notaHacer1 = notaHacer1;
+        this.notaHacer2 = notaHacer2;
+        this.notaHacer3 = notaHacer3;
+        this.notaHacer4 = notaHacer4;
+        this.notaHacer5 = notaHacer5;
+        this.notaHacer6 = notaHacer6;
+        this.notaDecir1 = notaDecir1;
+        this.notaDecir2 = notaDecir2;
+        this.notaDecir3 = notaDecir3;
+        this.notaDecir4 = notaDecir4;
+        this.notaDecir5 = notaDecir5;
+        this.notaDecir6 = notaDecir6;
+    }
+
+    public Long getCi() {
+        return ci;
+    }
+
+    public void setCi(Long ci) {
+        this.ci = ci;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaterno() {
+        return paterno;
+    }
+
+    public void setPaterno(String paterno) {
+        this.paterno = paterno;
+    }
+
+    public String getMaterno() {
+        return materno;
+    }
+
+    public void setMaterno(String materno) {
+        this.materno = materno;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Long telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Long getIdDocente() {
+        return idDocente;
+    }
+
+    public void setIdDocente(Long idDocente) {
+        this.idDocente = idDocente;
+    }
+
+    public Integer getBimestre() {
+        return bimestre;
+    }
+
+    public void setBimestre(Integer bimestre) {
+        this.bimestre = bimestre;
+    }
+
+    public String getParalelo() {
+        return paralelo;
+    }
+
+    public void setParalelo(String paralelo) {
+        this.paralelo = paralelo;
+    }
+
+    public Integer getGestion() {
+        return gestion;
+    }
+
+    public void setGestion(Integer gestion) {
+        this.gestion = gestion;
+    }
+
+    public Long getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(Long idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    public String getIndicadorCualitativo() {
+        return indicadorCualitativo;
+    }
+
+    public void setIndicadorCualitativo(String indicadorCualitativo) {
+        this.indicadorCualitativo = indicadorCualitativo;
+    }
+
+    public Double getNotaBimestralFinal() {
+        return notaBimestralFinal;
+    }
+
+    public void setNotaBimestralFinal(Double notaBimestralFinal) {
+        this.notaBimestralFinal = notaBimestralFinal;
+    }
+
+    public Double getPromedioAutoevaluacion() {
+        return promedioAutoevaluacion;
+    }
+
+    public void setPromedioAutoevaluacion(Double promedioAutoevaluacion) {
+        this.promedioAutoevaluacion = promedioAutoevaluacion;
+    }
+
+    public Double getAutoevaluacionDecir() {
+        return autoevaluacionDecir;
+    }
+
+    public void setAutoevaluacionDecir(Double autoevaluacionDecir) {
+        this.autoevaluacionDecir = autoevaluacionDecir;
+    }
+
+    public Double getAutoevaluacionSer() {
+        return autoevaluacionSer;
+    }
+
+    public void setAutoevaluacionSer(Double autoevaluacionSer) {
+        this.autoevaluacionSer = autoevaluacionSer;
+    }
+
+    public Double getDecirPromedio() {
+        return decirPromedio;
+    }
+
+    public void setDecirPromedio(Double decirPromedio) {
+        this.decirPromedio = decirPromedio;
+    }
+
+    public Double getHacerPromedio() {
+        return hacerPromedio;
+    }
+
+    public void setHacerPromedio(Double hacerPromedio) {
+        this.hacerPromedio = hacerPromedio;
+    }
+
+    public Double getSaberPromedio() {
+        return saberPromedio;
+    }
+
+    public void setSaberPromedio(Double saberPromedio) {
+        this.saberPromedio = saberPromedio;
+    }
+
+    public Double getSerPromedio() {
+        return serPromedio;
+    }
+
+    public void setSerPromedio(Double serPromedio) {
+        this.serPromedio = serPromedio;
+    }
+
+    public Integer getNotaSer1() {
+        return notaSer1;
+    }
+
+    public void setNotaSer1(Integer notaSer1) {
+        this.notaSer1 = notaSer1;
+    }
+
+    public Integer getNotaSer2() {
+        return notaSer2;
+    }
+
+    public void setNotaSer2(Integer notaSer2) {
+        this.notaSer2 = notaSer2;
+    }
+
+    public Integer getNotaSer3() {
+        return notaSer3;
+    }
+
+    public void setNotaSer3(Integer notaSer3) {
+        this.notaSer3 = notaSer3;
+    }
+
+    public Integer getNotaSer4() {
+        return notaSer4;
+    }
+
+    public void setNotaSer4(Integer notaSer4) {
+        this.notaSer4 = notaSer4;
+    }
+
+    public Integer getNotaSer5() {
+        return notaSer5;
+    }
+
+    public void setNotaSer5(Integer notaSer5) {
+        this.notaSer5 = notaSer5;
+    }
+
+    public Integer getNotaSer6() {
+        return notaSer6;
+    }
+
+    public void setNotaSer6(Integer notaSer6) {
+        this.notaSer6 = notaSer6;
+    }
+
+    public Integer getNotaSaber1() {
+        return notaSaber1;
+    }
+
+    public void setNotaSaber1(Integer notaSaber1) {
+        this.notaSaber1 = notaSaber1;
+    }
+
+    public Integer getNotaSaber2() {
+        return notaSaber2;
+    }
+
+    public void setNotaSaber2(Integer notaSaber2) {
+        this.notaSaber2 = notaSaber2;
+    }
+
+    public Integer getNotaSaber3() {
+        return notaSaber3;
+    }
+
+    public void setNotaSaber3(Integer notaSaber3) {
+        this.notaSaber3 = notaSaber3;
+    }
+
+    public Integer getNotaSaber4() {
+        return notaSaber4;
+    }
+
+    public void setNotaSaber4(Integer notaSaber4) {
+        this.notaSaber4 = notaSaber4;
+    }
+
+    public Integer getNotaSaber5() {
+        return notaSaber5;
+    }
+
+    public void setNotaSaber5(Integer notaSaber5) {
+        this.notaSaber5 = notaSaber5;
+    }
+
+    public Integer getNotaSaber6() {
+        return notaSaber6;
+    }
+
+    public void setNotaSaber6(Integer notaSaber6) {
+        this.notaSaber6 = notaSaber6;
+    }
+
+    public Integer getNotaHacer1() {
+        return notaHacer1;
+    }
+
+    public void setNotaHacer1(Integer notaHacer1) {
+        this.notaHacer1 = notaHacer1;
+    }
+
+    public Integer getNotaHacer2() {
+        return notaHacer2;
+    }
+
+    public void setNotaHacer2(Integer notaHacer2) {
+        this.notaHacer2 = notaHacer2;
+    }
+
+    public Integer getNotaHacer3() {
+        return notaHacer3;
+    }
+
+    public void setNotaHacer3(Integer notaHacer3) {
+        this.notaHacer3 = notaHacer3;
+    }
+
+    public Integer getNotaHacer4() {
+        return notaHacer4;
+    }
+
+    public void setNotaHacer4(Integer notaHacer4) {
+        this.notaHacer4 = notaHacer4;
+    }
+
+    public Integer getNotaHacer5() {
+        return notaHacer5;
+    }
+
+    public void setNotaHacer5(Integer notaHacer5) {
+        this.notaHacer5 = notaHacer5;
+    }
+
+    public Integer getNotaHacer6() {
+        return notaHacer6;
+    }
+
+    public void setNotaHacer6(Integer notaHacer6) {
+        this.notaHacer6 = notaHacer6;
+    }
+
+    public Integer getNotaDecir1() {
+        return notaDecir1;
+    }
+
+    public void setNotaDecir1(Integer notaDecir1) {
+        this.notaDecir1 = notaDecir1;
+    }
+
+    public Integer getNotaDecir2() {
+        return notaDecir2;
+    }
+
+    public void setNotaDecir2(Integer notaDecir2) {
+        this.notaDecir2 = notaDecir2;
+    }
+
+    public Integer getNotaDecir3() {
+        return notaDecir3;
+    }
+
+    public void setNotaDecir3(Integer notaDecir3) {
+        this.notaDecir3 = notaDecir3;
+    }
+
+    public Integer getNotaDecir4() {
+        return notaDecir4;
+    }
+
+    public void setNotaDecir4(Integer notaDecir4) {
+        this.notaDecir4 = notaDecir4;
+    }
+
+    public Integer getNotaDecir5() {
+        return notaDecir5;
+    }
+
+    public void setNotaDecir5(Integer notaDecir5) {
+        this.notaDecir5 = notaDecir5;
+    }
+
+    public Integer getNotaDecir6() {
+        return notaDecir6;
+    }
+
+    public void setNotaDecir6(Integer notaDecir6) {
+        this.notaDecir6 = notaDecir6;
+    }
+}
